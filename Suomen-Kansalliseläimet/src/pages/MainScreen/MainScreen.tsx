10d4901a9c6aaf8948233b357c9c87a126e5f9b3
Niklas Virtanen <93010689+Niklas20@users.noreply.github.com>
2024-09-03 09:27:59 +0300

    added start button to main screen, added cursor and pointer

--- Suomen-Kansalliseläimet/src/pages/MainScreen/MainScreen.tsx
+++ Suomen-Kansalliseläimet/src/pages/MainScreen/MainScreen.tsx
@@ -1,12 +1,24 @@
+import { useNavigate } from "react-router-dom";
 import { useTranslate } from "../../utils/translate";
 import "./MainScreen.css";
 
 const MainScreen = () => {
+    const navigate = useNavigate();
     const translate = useTranslate();
+
+    const handleButtonClick = () => {
+        navigate("/info");
+    }
 
     return (
         <div className="screen main-screen">
             <h1 className="main-screen-title">{translate("main-screen-title")}</h1>
+            <button
+                className="main-screen-button"
+                onClick={handleButtonClick}
+            >
+                {translate("main-screen-button")}
+            </button>
         </div>
     )
 }
