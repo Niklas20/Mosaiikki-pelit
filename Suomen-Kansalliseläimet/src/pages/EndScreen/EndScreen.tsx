0fe87d42037aa13f467e398e0cb874a2803b3a23
zylic <tomas.rasala@gmail.com>
2024-09-04 11:46:48 +0300

    End screen complete

--- Suomen-Kansalliseläimet/src/pages/EndScreen/EndScreen.tsx
+++ Suomen-Kansalliseläimet/src/pages/EndScreen/EndScreen.tsx
@@ -1,9 +1,62 @@
+import "./EndScreen.css";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import authorData from "../../data/AuthorData.json";
+import AuthorPopup from "../../components/EndScreen/AuthorPopup/AuthorPopup";
+import { useTranslate } from "../../utils/translate";
+
 const EndScreen = () => {
+    const [visibleAuthors, setVisibleAuthors] = useState<string | null>(null);
+
+    const translate = useTranslate();
+    const navigate = useNavigate();
+
+    const handleButtonClick = () => {
+        navigate("/game");
+    }
+
+    const openAuthorPopup = (authorName: string) => {
+        setVisibleAuthors(authorName);
+    }
+
+    const closeAuthorPopup = () => {
+        setVisibleAuthors(null);
+    }
+
+
     return (
         <div className="screen">
-            <h1>End Screen WIP...</h1>
+            <h1 className="end-screen-title">{translate("end-screen-title")}</h1>
+
+            <button
+                className="end-screen-restart-button"
+                onClick={handleButtonClick}
+            >
+                {translate("end-screen-restart-button")}
+            </button>
+
+            <div className="end-screen-author-container">
+                <h2 className="author-title">{translate("author-title")}</h2>
+                {authorData.authors.map(author => (
+                    <div
+                        className="author"
+                        key={author.name}
+                        onClick={() => openAuthorPopup(author.name)}
+                    >
+                        <p>{author.name}</p>
+                    </div>
+                ))}
+            </div>
+
+            {visibleAuthors && (
+                <AuthorPopup
+                    authorData={authorData.authors.find(author => author.name === visibleAuthors)!}
+                    onClose={closeAuthorPopup}
+                />
+            )}
+
         </div>
-    )
+    );
 }
 
 export default EndScreen;
--- Suomen-Kansalliseläimet/src/utils/types.ts
+++ Suomen-Kansalliseläimet/src/utils/types.ts
@@ -5,3 +5,23 @@ export interface Translations {
         [language in Language]: string;
     };
 }
+
+/**
+ * Author type
+ * 
+ * @param {string} name - Author name
+ * @param {string} school - Author school
+ * @param {string} degree - Author degree
+ * @param {string} email - Author email
+ * @param {string} github - Author GitHub
+ * @param {string} linkedin - Author LinkedIn
+ * @returns {Author} Author object
+ */
+export interface Author {
+    name: string;
+    school: string;
+    degree: string;
+    email: string;
+    github: string;
+    linkedin: string;
+}
