a395524b3da3845b323b140cac7e89b3da1d39f6
Niklas Virtanen <93010689+Niklas20@users.noreply.github.com>
2024-09-17 13:07:35 +0300

    added spinner that spins through random animals

--- Suomen-Kansalliseläimet/src/components/Spinner/Spinner.tsx
+++ Suomen-Kansalliseläimet/src/components/Spinner/Spinner.tsx
@@ -7,10 +7,49 @@ interface SpinnerProps {
 }
 
 const Spinner = ({ animals }: SpinnerProps) => {
-    const [isRolling, setIsRolling] = useState(false);
+    const [isRolling, setIsRolling] = useState<boolean>(false);
+
+    const generateItems = () => {
+        const items = document.querySelector(".items") as HTMLDivElement;
+        items.innerHTML = "";
+        items.style.transition = "none";
+        items.style.transform = "translateX(0)";
+
+        for (let i = 0; i < 30; i++) {
+            const item = document.createElement("div");
+            item.className = "item";
+
+            const randomIndex = Math.floor(Math.random() * animals.length);
+            const randomAnimal = animals[randomIndex];
+            const name = document.createElement("p");
+            name.className = "item-name";
+            name.innerText = randomAnimal.name;
+
+            item.appendChild(name);
+            items.appendChild(item);
+        }
+    };
 
     const startSpin = () => {
         setIsRolling(true);
+        generateItems();
+
+        const items = document.querySelector(".items") as HTMLDivElement;
+        const itemWrapper = document.querySelector(".item-wrapper") as HTMLDivElement;
+        const itemWidth = items.querySelector(".item")!.clientWidth;
+        const visibleWidth = itemWrapper.clientWidth;
+
+        const itemsCount = items.children.length;
+        const thirdLastIndex = itemsCount - 3;
+        const baseTranslateTo = itemWidth * thirdLastIndex - (visibleWidth / 2 - itemWidth / 2);
+
+        const randomOffset = Math.floor(Math.random() * 100) - 50;
+
+        const finalTranslateTo = baseTranslateTo + randomOffset;
+
+        items.style.transition = "transform 2s ease-in-out";
+        items.style.transform = `translateX(-${finalTranslateTo}px)`;
+
         setTimeout(() => {
             setIsRolling(false);
         }, 2000);
@@ -18,10 +57,14 @@ const Spinner = ({ animals }: SpinnerProps) => {
 
     return (
         <div className="spinner-container">
-            <div className="items">
+            <div className="item-wrapper">
+                <div className="items">
 
+                </div>
             </div>
-            <button className="spin-button" onClick={startSpin} disabled={isRolling}>{isRolling ? "Spinning..." : "Spin"}</button>
+            <button className="spin-button" onClick={startSpin} disabled={isRolling}>
+                {isRolling ? "Spinning..." : "Spin"}
+            </button>
         </div>
     );
 };
